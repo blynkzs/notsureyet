6f69247ddf66834834006c19959ef9bd31d787ce
blynkzs <dustinp2003@gmail.com>
2025-07-18 20:07:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,31 +29,26 @@ const pieceUnicode = {
 
 function isUpper(c) { return c === c.toUpperCase(); }
 function isLower(c) { return c === c.toLowerCase(); }
-function isEmpty(r, c) { return board[r][c] === ''; }
-function oppositeColor(piece1, piece2) {
-  return (isUpper(piece1) && isLower(piece2)) || (isLower(piece1) && isUpper(piece2));
+function onBoard(r, c) { return r >= 0 && r < 8 && c >= 0 && c < 8; }
+function opposite(color) { return color === 'white' ? 'black' : 'white'; }
+function oppositeColor(p1, p2) {
+  return (isUpper(p1) && isLower(p2)) || (isLower(p1) && isUpper(p2));
 }
 
-function onBoard(r, c) {
-  return r >=0 && r < 8 && c >=0 && c < 8;
-}
-
-const rookDirs = [[1,0],[-1,0],[0,1],[0,-1]];
-const bishopDirs = [[1,1],[1,-1],[-1,1],[-1,-1]];
-const knightMoves = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]];
-
+// Track kings' positions and castling rights
 let kingPositions = { white: [7,4], black: [0,4] };
 let castlingRights = {
   white: { kingside: true, queenside: true },
   black: { kingside: true, queenside: true }
 };
-let halfMoveClock = 0;
-let fullMoveNumber = 1;
+
+const rookDirs = [[1,0],[-1,0],[0,1],[0,-1]];
+const bishopDirs = [[1,1],[1,-1],[-1,1],[-1,-1]];
+const knightMoves = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]];
 
 function generateMoves(r, c) {
   const piece = board[r][c];
   if (!piece) return [];
-
   const moves = [];
   const isWhite = isUpper(piece);
 
@@ -67,26 +62,24 @@ function generateMoves(r, c) {
     }
   }
 
-  switch (piece.toLowerCase()) {
+  switch(piece.toLowerCase()) {
     case 'p': {
       const dir = isWhite ? -1 : 1;
       const startRow = isWhite ? 6 : 1;
 
-      // Forward one
+      // Forward moves
       if (onBoard(r+dir, c) && board[r+dir][c] === '') {
         moves.push([r+dir, c]);
-        // Forward two from start
         if (r === startRow && board[r+2*dir][c] === '') {
           moves.push([r+2*dir, c]);
         }
       }
+
       // Captures
       for (const dc of [-1,1]) {
         const tr = r+dir, tc = c+dc;
-        if (onBoard(tr, tc)) {
-          if (board[tr][tc] !== '' && oppositeColor(piece, board[tr][tc])) {
-            moves.push([tr, tc]);
-          }
+        if (onBoard(tr, tc) && board[tr][tc] !== '' && oppositeColor(piece, board[tr][tc])) {
+          moves.push([tr, tc]);
         }
       }
       break;
@@ -105,10 +98,9 @@ function generateMoves(r, c) {
     case 'b': {
       for (const [dr, dc] of bishopDirs) {
         let tr = r+dr, tc = c+dc;
-        while (onBoard(tr, tc)) {
-          if (board[tr][tc] === '') {
-            moves.push([tr, tc]);
-          } else {
+        while(onBoard(tr, tc)) {
+          if (board[tr][tc] === '') moves.push([tr, tc]);
+          else {
             if (oppositeColor(piece, board[tr][tc])) moves.push([tr, tc]);
             break;
           }
@@ -120,10 +112,9 @@ function generateMoves(r, c) {
     case 'r': {
       for (const [dr, dc] of rookDirs) {
         let tr = r+dr, tc = c+dc;
-        while (onBoard(tr, tc)) {
-          if (board[tr][tc] === '') {
-            moves.push([tr, tc]);
-          } else {
+        while(onBoard(tr, tc)) {
+          if (board[tr][tc] === '') moves.push([tr, tc]);
+          else {
             if (oppositeColor(piece, board[tr][tc])) moves.push([tr, tc]);
             break;
           }
@@ -135,10 +126,9 @@ function generateMoves(r, c) {
     case 'q': {
       for (const [dr, dc] of [...rookDirs, ...bishopDirs]) {
         let tr = r+dr, tc = c+dc;
-        while (onBoard(tr, tc)) {
-          if (board[tr][tc] === '') {
-            moves.push([tr, tc]);
-          } else {
+        while(onBoard(tr, tc)) {
+          if (board[tr][tc] === '') moves.push([tr, tc]);
+          else {
             if (oppositeColor(piece, board[tr][tc])) moves.push([tr, tc]);
             break;
           }
@@ -148,33 +138,33 @@ function generateMoves(r, c) {
       break;
     }
     case 'k': {
-      for (let dr = -1; dr <= 1; dr++) {
-        for (let dc = -1; dc <= 1; dc++) {
+      for (let dr = -1; dr <=1; dr++) {
+        for (let dc = -1; dc <=1; dc++) {
           if (dr === 0 && dc === 0) continue;
           const tr = r+dr, tc = c+dc;
           if (!onBoard(tr, tc)) continue;
           const target = board[tr][tc];
-          if (target === '' || oppositeColor(piece, target)) {
-            moves.push([tr, tc]);
-          }
+          if (target === '' || oppositeColor(piece, target)) moves.push([tr, tc]);
         }
       }
       if (!isInCheck(turn)) {
         if (castlingRights[turn].kingside && canCastleKingside(turn)) {
-          moves.push([turn === 'white' ? 7 : 0, 6]);
+          moves.push([turn==='white'?7:0, 6]);
         }
         if (castlingRights[turn].queenside && canCastleQueenside(turn)) {
-          moves.push([turn === 'white' ? 7 : 0, 2]);
+          moves.push([turn==='white'?7:0, 2]);
         }
       }
       break;
     }
   }
+
+  // Filter out moves that leave king in check
   return moves.filter(m => !wouldBeCheck(r, c, m[0], m[1]));
 }
 
 function canCastleKingside(color) {
-  const r = color === 'white' ? 7 : 0;
+  const r = color==='white'?7:0;
   if (board[r][5] !== '' || board[r][6] !== '') return false;
   if (isSquareAttacked(r, 4, opposite(color))) return false;
   if (isSquareAttacked(r, 5, opposite(color))) return false;
@@ -183,7 +173,7 @@ function canCastleKingside(color) {
 }
 
 function canCastleQueenside(color) {
-  const r = color === 'white' ? 7 : 0;
+  const r = color==='white'?7:0;
   if (board[r][1] !== '' || board[r][2] !== '' || board[r][3] !== '') return false;
   if (isSquareAttacked(r, 4, opposite(color))) return false;
   if (isSquareAttacked(r, 3, opposite(color))) return false;
@@ -192,15 +182,13 @@ function canCastleQueenside(color) {
 }
 
 function isSquareAttacked(r, c, byColor) {
-  for (let rr = 0; rr < 8; rr++) {
-    for (let cc = 0; cc < 8; cc++) {
+  for (let rr=0; rr<8; rr++) {
+    for (let cc=0; cc<8; cc++) {
       const piece = board[rr][cc];
       if (!piece) continue;
-      if ((byColor === 'white' && isUpper(piece)) || (byColor === 'black' && isLower(piece))) {
+      if ((byColor==='white' && isUpper(piece)) || (byColor==='black' && isLower(piece))) {
         const moves = generatePseudoMoves(rr, cc);
-        for (const [mr, mc] of moves) {
-          if (mr === r && mc === c) return true;
-        }
+        if (moves.some(m => m[0]===r && m[1]===c)) return true;
       }
     }
   }
@@ -208,29 +196,28 @@ function isSquareAttacked(r, c, byColor) {
 }
 
 function generatePseudoMoves(r, c) {
-  // Similar to generateMoves but without check filtering to avoid recursion
+  // Like generateMoves but ignores checks to avoid recursion
   const piece = board[r][c];
   if (!piece) return [];
   const moves = [];
   const isWhite = isUpper(piece);
 
-  function tryMove(tr, tc, canCapture = true) {
+  function tryMove(tr, tc, canCapture=true) {
     if (!onBoard(tr, tc)) return;
     const target = board[tr][tc];
-    if (target === '') {
-      moves.push([tr, tc]);
-    } else if (canCapture && oppositeColor(piece, target)) {
-      moves.push([tr, tc]);
-    }
+    if (target === '') moves.push([tr, tc]);
+    else if (canCapture && oppositeColor(piece, target)) moves.push([tr, tc]);
   }
 
-  switch (piece.toLowerCase()) {
+  switch(piece.toLowerCase()) {
     case 'p': {
       const dir = isWhite ? -1 : 1;
       if (onBoard(r+dir, c) && board[r+dir][c] === '') moves.push([r+dir, c]);
       for (const dc of [-1,1]) {
         const tr = r+dir, tc = c+dc;
-        if (onBoard(tr, tc) && board[tr][tc] !== '' && oppositeColor(piece, board[tr][tc])) moves.push([tr, tc]);
+        if (onBoard(tr, tc) && board[tr][tc] !== '' && oppositeColor(piece, board[tr][tc])) {
+          moves.push([tr, tc]);
+        }
       }
       break;
     }
@@ -246,7 +233,7 @@ function generatePseudoMoves(r, c) {
     case 'b': {
       for (const [dr, dc] of bishopDirs) {
         let tr = r+dr, tc = c+dc;
-        while (onBoard(tr, tc)) {
+        while(onBoard(tr, tc)) {
           if (board[tr][tc] === '') moves.push([tr, tc]);
           else {
             if (oppositeColor(piece, board[tr][tc])) moves.push([tr, tc]);
@@ -260,7 +247,7 @@ function generatePseudoMoves(r, c) {
     case 'r': {
       for (const [dr, dc] of rookDirs) {
         let tr = r+dr, tc = c+dc;
-        while (onBoard(tr, tc)) {
+        while(onBoard(tr, tc)) {
           if (board[tr][tc] === '') moves.push([tr, tc]);
           else {
             if (oppositeColor(piece, board[tr][tc])) moves.push([tr, tc]);
@@ -274,7 +261,7 @@ function generatePseudoMoves(r, c) {
     case 'q': {
       for (const [dr, dc] of [...rookDirs, ...bishopDirs]) {
         let tr = r+dr, tc = c+dc;
-        while (onBoard(tr, tc)) {
+        while(onBoard(tr, tc)) {
           if (board[tr][tc] === '') moves.push([tr, tc]);
           else {
             if (oppositeColor(piece, board[tr][tc])) moves.push([tr, tc]);
@@ -286,9 +273,9 @@ function generatePseudoMoves(r, c) {
       break;
     }
     case 'k': {
-      for (let dr = -1; dr <= 1; dr++) {
-        for (let dc = -1; dc <= 1; dc++) {
-          if (dr === 0 && dc === 0) continue;
+      for (let dr=-1; dr<=1; dr++) {
+        for (let dc=-1; dc<=1; dc++) {
+          if (dr===0 && dc===0) continue;
           const tr = r+dr, tc = c+dc;
           if (!onBoard(tr, tc)) continue;
           const target = board[tr][tc];
@@ -308,31 +295,28 @@ function wouldBeCheck(sr, sc, tr, tc) {
   board[tr][tc] = piece;
   board[sr][sc] = '';
 
+  // Update king pos if moving king
+  let origKingPos = null;
   if (piece.toLowerCase() === 'k') {
+    origKingPos = kingPositions[turn];
     kingPositions[turn] = [tr, tc];
-  } else if (piece.toLowerCase() === 'k' && turn === 'black') {
-    kingPositions.black = [tr, tc];
   }
 
-  const check = isInCheck(turn);
+  const inCheck = isInCheck(turn);
 
   board[sr][sc] = piece;
   board[tr][tc] = target;
 
-  if (piece.toLowerCase() === 'k') {
-    kingPositions[turn] = [sr, sc];
+  if (piece.toLowerCase() === 'k' && origKingPos) {
+    kingPositions[turn] = origKingPos;
   }
 
-  return check;
+  return inCheck;
 }
 
 function isInCheck(color) {
   const [kr, kc] = kingPositions[color];
   return isSquareAttacked(kr, kc, opposite(color));
-}
-
-function opposite(color) {
-  return color === 'white' ? 'black' : 'white';
 }
 
 function canMove(sr, sc, tr, tc) {
@@ -341,8 +325,7 @@ function canMove(sr, sc, tr, tc) {
   const target = board[tr][tc];
   if (target && ((isUpper(piece) && isUpper(target)) || (isLower(piece) && isLower(target)))) return false;
 
-  const moves = generateMoves(sr, sc);
-  return moves.some(m => m[0] === tr && m[1] === tc);
+  return generateMoves(sr, sc).some(m => m[0] === tr && m[1] === tc);
 }
 
 function movePiece(sr, sc, tr, tc) {
@@ -361,28 +344,27 @@ function movePiece(sr, sc, tr, tc) {
     else captured.white.push(target);
   }
 
-  // Castling move
+  // Castling
   if (piece.toLowerCase() === 'k' && Math.abs(tc - sc) === 2) {
+    const r = sr;
     if (tc === 6) { // kingside
-      board[tr][5] = board[tr][7];
-      board[tr][7] = '';
+      board[r][5] = board[r][7];
+      board[r][7] = '';
     } else if (tc === 2) { // queenside
-      board[tr][3] = board[tr][0];
-      board[tr][0] = '';
+      board[r][3] = board[r][0];
+      board[r][0] = '';
     }
   }
 
   board[tr][tc] = piece;
   board[sr][sc] = '';
 
-  // Update king position
   if (piece.toLowerCase() === 'k') {
     kingPositions[turn] = [tr, tc];
     castlingRights[turn].kingside = false;
     castlingRights[turn].queenside = false;
   }
 
-  // Rook moves cancel castling rights
   if (piece.toLowerCase() === 'r') {
     if (sr === 7 && sc === 0) castlingRights.white.queenside = false;
     if (sr === 7 && sc === 7) castlingRights.white.kingside = false;
@@ -390,14 +372,14 @@ function movePiece(sr, sc, tr, tc) {
     if (sr === 0 && sc === 7) castlingRights.black.kingside = false;
   }
 
-  // Pawn promotion (auto promote to queen)
+  // Pawn promotion auto to queen
   if (piece.toLowerCase() === 'p' && (tr === 0 || tr === 7)) {
     board[tr][tc] = isUpper(piece) ? 'Q' : 'q';
   }
 
-  // Reset selected and legal moves after move
   selected = null;
   legalMoves = [];
+  turn = opposite(turn);
 }
 
 function updateCaptured() {
@@ -459,4 +441,7 @@ function renderBoard() {
   for(let r=0; r<8; r++) {
     for(let c=0; c<8; c++) {
       const square = document.createElement('div');
-      const isLight = (r + c) % 2 ===
+      const isLight = (r + c) % 2 === 0;
+      square.className = 'square ' + (isLight ? 'light' : 'dark');
+      square.style.width = '60px';
+      square.style.height = '60px
